e90a932d930602cfaf7aa633379304c942e2836a
Антон <antonbliznuk71@gmail.com>
2025-07-14 13:32:19 +0300

    Implemented views for "User" model and connected them in users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,24 @@
 from django.urls import path
+from users.views import (
+    CreateUserView,
+    ManageUserView,
+    LogoutUserView,
+    ThrottledLoginView,
+)
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
+    TokenVerifyView,
 )
 
 
-app_name = "users"
+app_name = "user"
 
 urlpatterns = [
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("register/", CreateUserView.as_view(), name="user-register"),
+    path("me/", ManageUserView.as_view(), name="user-me"),
+    path("logout/", LogoutUserView.as_view(), name="user-logout"),
+
+    path("token/", ThrottledLoginView.as_view(), name="token-obtain-pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token-verify"),
 ]
