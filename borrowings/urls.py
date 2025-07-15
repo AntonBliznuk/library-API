73175d082c8c9800d6f462487f841ca1e9c8605b
Антон <antonbliznuk71@gmail.com>
2025-07-15 10:24:44 +0300

    Implemented BorrowingViewSet and connected it in borrowings/urls.py

--- borrowings/urls.py
+++ borrowings/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
+from borrowings.views import BorrowingViewSet
+
+
+router = routers.DefaultRouter()
+router.register("borrowings", BorrowingViewSet)
 
 app_name = "borrowings"
 
-urlpatterns = []
+urlpatterns = [
+    path("", include(router.urls)),
+]
--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from borrowings.models import Borrowing
+from borrowings.serializers import (
+    BorrowingListSerializer,
+    BorrowingRetrieveSerializer,
+)
+
+
+class BorrowingViewSet(viewsets.ModelViewSet):
+    queryset = Borrowing.objects.select_related(
+        "user", "book"
+    )
+
+    def get_serializer_class(self):
+        if self.action in {"retrieve", "update", "partial_update"}:
+            return BorrowingRetrieveSerializer
+        return BorrowingListSerializer
