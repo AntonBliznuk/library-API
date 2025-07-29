83bc197c4d22259f8fe72a04c510f4bbf1eab64a
Антон <antonbliznuk71@gmail.com>
2025-07-29 14:22:14 +0300

    Implemented create_stripe_fine_payment function to create payments for fines.

--- utils/stripe.py
+++ utils/stripe.py
@@ -46,3 +46,42 @@ def create_stripe_session_for_borrowing(borrowing, request):
     payment.save()
 
     return payment
+
+
+def create_stripe_fine_payment(borrowing, fine_amount, request):
+    payment = Payment.objects.create(
+        borrowing=borrowing,
+        payment_status=Payment.PaymentStatusChoices.PENDING,
+        payment_type=Payment.PaymentTypeChoices.FINE,
+        usd_to_pay=fine_amount,
+    )
+
+    success_url = request.build_absolute_uri(
+        reverse("borrowings:payment-success-payment", kwargs={"pk": payment.id})
+    ) + "?session_id={CHECKOUT_SESSION_ID}"
+
+    cancel_url = request.build_absolute_uri(
+        reverse("borrowings:payment-cancel-payment", kwargs={"pk": payment.id})
+    )
+    session = stripe.checkout.Session.create(
+        payment_method_types=["card"],
+        line_items=[{
+            "price_data": {
+                "currency": "usd",
+                "product_data": {
+                    "name": f"Fine for overdue book: {borrowing.book.title}"
+                },
+                "unit_amount": int(fine_amount * 100),
+            },
+            "quantity": 1,
+        }],
+        mode="payment",
+        success_url=success_url,
+        cancel_url=cancel_url,
+    )
+
+    payment.session_id = session.id
+    payment.session_url = session.url
+    payment.save()
+
+    return payment
